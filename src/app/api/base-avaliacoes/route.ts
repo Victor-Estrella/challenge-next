8fe7ba399412a4ffe7a464d30791c2226cb96443
Victor-Estrella <victorhcarracci@gmail.com>
2024-10-26 00:59:25 -0300

    Criação carrossel

--- src/app/api/base-avaliacoes/route.ts
+++ src/app/api/base-avaliacoes/route.ts
@@ -2,13 +2,13 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
 
-    //REcuperando os dados da API JAVA
+    //Recuperando os dados da API JAVA
     try {
         const response = await fetch("http://192.168.0.13:8080/avaliacao");
         const data = await response.json();
         return NextResponse.json(data);
     } catch (error) {
-        console.error("Não consegui puxar os dados do estrela!", error);
-        return NextResponse.json({error:"Erro nos dados do estrela :" + error});
+        console.error("Não consegui puxar os dados da avaliação!", error);
+        return NextResponse.json({error:"Erro nos dados da avaliação :" + error});
     }
 }
--- src/components/Avaliacao/Avaliacao.tsx
+++ src/components/Avaliacao/Avaliacao.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 export default function Avaliacao() {
   const [lista, setLista] = useState<TipoAvaliacao[]>([]);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isMobileView, setIsMobileView] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,42 +20,59 @@ export default function Avaliacao() {
     };
 
     fetchData();
+
+    const handleResize = () => {
+      setIsMobileView(window.innerWidth < 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  // Função para renderizar as estrelas
   const renderStars = (avaliacao: number) => {
-    const stars = [];
-    for (let i = 0; i < avaliacao; i++) {
-      stars.push(
-        <svg key={i} className="w-6 h-6 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 19" fill="none">
-          <path d="M3.825 19L5.45 11.975L0 7.25L7.2 6.625L10 0L12.8 6.625L20 7.25L14.55 11.975L16.175 19L10 15.275L3.825 19Z" fill="#01A1FC"/>
-        </svg>
-      );
-    }
-    return stars;
+    return Array.from({ length: avaliacao }, (_, i) => (
+      <svg key={i} className="w-6 h-6 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 19" fill="none">
+        <path d="M3.825 19L5.45 11.975L0 7.25L7.2 6.625L10 0L12.8 6.625L20 7.25L14.55 11.975L16.175 19L10 15.275L3.825 19Z" fill="#01A1FC"/>
+      </svg>
+    ));
   };
-
- 
-  const itemsPerPage = 4;
-  const totalPages = lista.length
 
   const next = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % totalPages);
+    setCurrentIndex((prevIndex) => {
+      const lastIndex = isMobileView ? lista.length - 1 : lista.length/2 - 1;
+      return prevIndex === lastIndex ? 0 : prevIndex + 1;
+    });
   };
 
+console.log(currentIndex)
+console.log(lista.length)
+
   const prev = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? totalPages - 1 : prevIndex - 1));
+    setCurrentIndex((prevIndex) => {
+      const lastIndex = isMobileView ? lista.length - 1 : lista.length/2 - 1;
+      return prevIndex === 0 ? lastIndex : prevIndex - 1;
+    });
   };
 
   return (
     <section className="flex flex-col items-center mb-32">
       <div className="flex justify-center w-full">
-        <h1 className="text-4xl font-bold mt-6 mb-10 text-azulclaro min-[460px]:text-5xl md:text-6xl lg:text-7xl lg:mt-10">O que nossos clientes acham</h1>
+        <h1 className="mx-4 text-2xl font-bold mt-6 mb-10 text-azulclaro min-[460px]:text-4xl md:text-5xl lg:text-6xl lg:mt-10">
+          O que nossos clientes acham
+        </h1>
       </div>
       <div className="relative w-full overflow-hidden">
-        <div className="flex transition-transform duration-300" style={{ transform: `translateX(-${currentIndex * (100 / itemsPerPage)}%)` }}>
-          {lista.map((avaliacao) => (
-            <div key={avaliacao.idAvaliacao} className="flex-shrink-0 w-full md:w-1/4 p-2"> {/* Definindo a largura */}
+        <div
+          className="flex transition-transform duration-300"
+          style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+        >
+          {(isMobileView ? lista.concat(lista) : lista).map((avaliacao, index) => (
+            <div
+              key={`${avaliacao.idAvaliacao}-${index}`}
+              className="flex-shrink-0 w-full md:w-1/2 p-2"
+            >
               <ul className="flex flex-col bg-[#DADADA] p-4 rounded-lg mb-6">
                 <div className="flex items-center gap-5">
                   <li className="text-[#001D5C] font-bold text-2xl">{avaliacao.nomeCliente}</li>
@@ -65,8 +83,10 @@ export default function Avaliacao() {
             </div>
           ))}
         </div>
-        <button onClick={prev} className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-white p-2 rounded">Anterior</button>
-        <button onClick={next} className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-white p-2 rounded">Próximo</button>
+        <div className="w-full flex justify-between mt-4">
+          <button onClick={prev} className="bg-white p-2 rounded ml-4">Anterior</button>
+          <button onClick={next} className="bg-white p-2 rounded mr-4">Próximo</button>
+        </div>
       </div>
     </section>
   );
